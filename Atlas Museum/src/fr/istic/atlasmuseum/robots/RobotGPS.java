7b3cb642f9424f574087c4cdaa522d271292a601
Thibault Gauthier <moithibault@gmail.com>
2015-01-15 11:10:36 +0100

    Ajout d'un d à adress pour que le paramètre soit bon.

--- Atlas Museum/src/fr/istic/atlasmuseum/robots/RobotGPS.java
+++ Atlas Museum/src/fr/istic/atlasmuseum/robots/RobotGPS.java
@@ -7,7 +7,7 @@ public class RobotGPS implements Robot{
 
 	private static final String url = "http://maps.googleapis.com/maps/api/geocode/xml";
 	public RobotGPS() {
-		String[] result = ParserGPS.analyseAnswer(Requestor.get(url+"?adress=vienne&sensor=false"));
-		System.out.println("Latitude : "+result[0]+ "longitude : "+result[1]);
+		String[] result = ParserGPS.analyseAnswer(Requestor.get(url+"?address=Rennes&sensor=false"));
+		System.out.println("Latitude : "+result[0]+ "\nlongitude : "+result[1]);
 	}
 }
