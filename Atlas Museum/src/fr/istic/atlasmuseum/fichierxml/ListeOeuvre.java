4de3819ab31f13c02169ddc364e6e934423e7566
TIADEMA <TIADEMA@148.60.14.78>
2015-01-20 10:32:47 +0100

    @init

---
+++ Atlas Museum/src/fr/istic/atlasmuseum/fichierxml/ListeOeuvre.java
@@ -0,0 +1,177 @@
+package fr.istic.atlasmuseum.fichierxml;
+import java.util.Date;
+
+
+public class ListeOeuvre {
+
+	public String getDate_de_presentation_devant_la_commission() {
+		return Date_de_presentation_devant_la_commission;
+	}
+	public void setDate_de_presentation_devant_la_commission(
+			String date_de_presentation_devant_la_commission) {
+		Date_de_presentation_devant_la_commission = date_de_presentation_devant_la_commission;
+	}
+	public String getDate_de_signature_de_l_arret_d_agrement() {
+		return Date_de_signature_de_l_arret_d_agrement;
+	}
+	public void setDate_de_signature_de_l_arret_d_agrement(
+			String date_de_signature_de_l_arret_d_agrement) {
+		Date_de_signature_de_l_arret_d_agrement = date_de_signature_de_l_arret_d_agrement;
+	}
+	private String Cote_du_dossier_aux_archives_du_MCC ;
+	private String Pays; 
+	private String Region;
+	private String Departement;
+	private String Commune;
+	private String Nom_de_l_etablissement;
+	private String Maitre_d_ouvrage_actuel;
+	private String Nom_de_l_artiste;
+	private String Prenom_de_l_artiste;
+	private String Type_de_commission;
+	private String   Date_de_presentation_devant_la_commission; 
+	private String Periode;
+	private String   Date_de_signature_de_l_arret_d_agrement;
+	private String Annee_installation_de_l_oeuvre;
+	private String Observations;
+	private String Realise;
+	private String Source_s;
+	private String Titre_de_l_oeuvre;
+	private String Descriptif_de_l_oeuvre;
+	private String Photographie_de_l_oeuvre_oui;
+	private String Nom_de_l_architecte;
+	private String Prenom_de_l_architecte;
+	private String Universite_Enseignement_sup_rieur;
+	
+
+	public String getCote_du_dossier_aux_archives_du_MCC() {
+		return Cote_du_dossier_aux_archives_du_MCC;
+	}
+	public void setCote_du_dossier_aux_archives_du_MCC(
+			String cote_du_dossier_aux_archives_du_MCC) {
+		Cote_du_dossier_aux_archives_du_MCC = cote_du_dossier_aux_archives_du_MCC;
+	}
+	public String getPays() {
+		return Pays;
+	}
+	public void setPays(String pays) {
+		Pays = pays;
+	}
+	public String getRegion() {
+		return Region;
+	}
+	public void setRegion(String region) {
+		Region = region;
+	}
+	public String getDepartement() {
+		return Departement;
+	}
+	public void setDepartement(String departement) {
+		Departement = departement;
+	}
+	public String getCommune() {
+		return Commune;
+	}
+	public void setCommune(String commune) {
+		Commune = commune;
+	}
+	public String getNom_de_l_etablissement() {
+		return Nom_de_l_etablissement;
+	}
+	public void setNom_de_l_etablissement(String nom_de_l_etablissement) {
+		Nom_de_l_etablissement = nom_de_l_etablissement;
+	}
+	public String getMaitre_d_ouvrage_actuel() {
+		return Maitre_d_ouvrage_actuel;
+	}
+	public void setMaitre_d_ouvrage_actuel(String maitre_d_ouvrage_actuel) {
+		Maitre_d_ouvrage_actuel = maitre_d_ouvrage_actuel;
+	}
+	public String getNom_de_l_artiste() {
+		return Nom_de_l_artiste;
+	}
+	public void setNom_de_l_artiste(String nom_de_l_artiste) {
+		Nom_de_l_artiste = nom_de_l_artiste;
+	}
+	public String getPrenom_de_l_artiste() {
+		return Prenom_de_l_artiste;
+	}
+	public void setPrenom_de_l_artiste(String prenom_de_l_artiste) {
+		Prenom_de_l_artiste = prenom_de_l_artiste;
+	}
+	public String getType_de_commission() {
+		return Type_de_commission;
+	}
+	public void setType_de_commission(String type_de_commission) {
+		Type_de_commission = type_de_commission;
+	}
+	
+	public String getPeriode() {
+		return Periode;
+	}
+	public void setPeriode(String periode) {
+		Periode = periode;
+	}
+	
+	public String getAnnee_installation_de_l_oeuvre() {
+		return Annee_installation_de_l_oeuvre;
+	}
+	public void setAnnee_installation_de_l_oeuvre(
+			String annee_installation_de_l_oeuvre) {
+		Annee_installation_de_l_oeuvre = annee_installation_de_l_oeuvre;
+	}
+	public String getObservations() {
+		return Observations;
+	}
+	public void setObservations(String observations) {
+		Observations = observations;
+	}
+	public String getRealise() {
+		return Realise;
+	}
+	public void setRealise(String realise) {
+		Realise = realise;
+	}
+	public String getSource_s() {
+		return Source_s;
+	}
+	public void setSource_s(String source_s) {
+		Source_s = source_s;
+	}
+	public String getTitre_de_l_oeuvre() {
+		return Titre_de_l_oeuvre;
+	}
+	public void setTitre_de_l_oeuvre(String titre_de_l_oeuvre) {
+		Titre_de_l_oeuvre = titre_de_l_oeuvre;
+	}
+	public String getDescriptif_de_l_oeuvre() {
+		return Descriptif_de_l_oeuvre;
+	}
+	public void setDescriptif_de_l_oeuvre(String descriptif_de_l_oeuvre) {
+		Descriptif_de_l_oeuvre = descriptif_de_l_oeuvre;
+	}
+	public String getPhotographie_de_l_oeuvre_oui() {
+		return Photographie_de_l_oeuvre_oui;
+	}
+	public void setPhotographie_de_l_oeuvre_oui(String photographie_de_l_oeuvre_oui) {
+		Photographie_de_l_oeuvre_oui = photographie_de_l_oeuvre_oui;
+	}
+	public String getNom_de_l_architecte() {
+		return Nom_de_l_architecte;
+	}
+	public void setNom_de_l_architecte(String nom_de_l_architecte) {
+		Nom_de_l_architecte = nom_de_l_architecte;
+	}
+	public String getPrenom_de_l_architecte() {
+		return Prenom_de_l_architecte;
+	}
+	public void setPrenom_de_l_architecte(String prenom_de_l_architecte) {
+		Prenom_de_l_architecte = prenom_de_l_architecte;
+	}
+	public String getUniversite_Enseignement_sup_rieur() {
+		return Universite_Enseignement_sup_rieur;
+	}
+	public void setUniversite_Enseignement_sup_rieur(
+			String universite_Enseignement_sup_rieur) {
+		Universite_Enseignement_sup_rieur = universite_Enseignement_sup_rieur;
+	}
+}
---
+++ Atlas Museum/src/fr/istic/atlasmuseum/fichierxml/Parseur.java
@@ -0,0 +1,196 @@
+package fr.istic.atlasmuseum.fichierxml;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class Parseur  extends DefaultHandler{
+	
+	private InputStream FIS;
+	private SAXParser saxParser;
+	private StringBuilder buffer;
+	
+	private ArrayList<ListeOeuvre> oeuvre= new ArrayList<ListeOeuvre>();
+
+	//	private HashMap<String,String> dico;
+	private String key;
+	private String original;
+	
+	public Parseur (String original){
+		this.original = original;
+			try {
+			// Creation d'un parser SAX
+			saxParser = SAXParserFactory.newInstance().newSAXParser();
+			// Ouverture du fichier XML
+			FIS= new FileInputStream(original+".xml");
+			
+			//FIS = getClass().getResourceAsStream(original+".xml");
+			// Lecture du fichier XML
+			saxParser.parse(FIS, this);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 			
+	}
+	
+	public Parseur (){
+	//	liArtistes = new HashMap<String,String>();
+		try {
+			// Creation d'un parser SAX
+			saxParser = SAXParserFactory.newInstance().newSAXParser();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 			
+	}
+	
+		@Override
+	public void characters(char[] ch, int start, int length)
+		throws SAXException {
+			if (buffer != null)
+				buffer.append(new String(ch,start,length));
+	}
+
+		@Override
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		int indice=oeuvre.size();
+			if(indice>0)
+			indice--;
+			
+			if (qName.equals("Cote_du_dossier_aux_archives_du_MCC"))
+				oeuvre.get(indice).setCote_du_dossier_aux_archives_du_MCC(buffer.toString());
+			
+			else if(qName.equals("Pays"))
+				oeuvre.get(indice).setPays(buffer.toString());
+			
+			else if (qName.equals("Region"))
+				oeuvre.get(indice).setRegion(buffer.toString());
+			
+			else if (qName.equals("Departement"))
+				oeuvre.get(indice).setDepartement(buffer.toString());
+				
+			else if (qName.equals("Commune"))
+				oeuvre.get(indice).setCommune(buffer.toString());
+				
+			else if (qName.equals("Nom_de_l_etablissement"))
+				oeuvre.get(indice).setNom_de_l_etablissement(buffer.toString());
+				
+			else if (qName.equals("Maitre_d_ouvrage_actuel"))
+				oeuvre.get(indice).setMaitre_d_ouvrage_actuel(buffer.toString());
+				
+			else if (qName.equals("Nom_de_l_artiste"))
+				oeuvre.get(indice).setNom_de_l_artiste(buffer.toString());
+				
+			else if (qName.equals("Prenom_de_l_artiste"))
+				oeuvre.get(indice).setPrenom_de_l_artiste(buffer.toString());
+				
+			else if(qName.equals("Type_de_commission"))
+				oeuvre.get(indice).setType_de_commission(buffer.toString());
+				
+			else if(qName.equals("Date_de_presentation_devant_la_commission"))
+				oeuvre.get(indice).setDate_de_presentation_devant_la_commission((buffer.toString()));	
+				
+			else if(qName.equals("Periode"))
+				oeuvre.get(indice).setPeriode(buffer.toString());	
+						
+			else if(qName.equals("Date_de_signature_de_l_arret_d_agrement"))
+				oeuvre.get(indice).setDate_de_signature_de_l_arret_d_agrement(buffer.toString());	
+				
+			else if(qName.equals("Annee_installation_de_l_oeuvre"))
+				oeuvre.get(indice).setAnnee_installation_de_l_oeuvre(buffer.toString());
+				
+			else if(qName.equals("Observations"))	
+				oeuvre.get(indice).setObservations(buffer.toString());
+				
+			else if(qName.equals("Realise"))
+				oeuvre.get(indice).setRealise(buffer.toString());
+				
+			else if(qName.equals("Source_s"))	
+				oeuvre.get(indice).setSource_s(buffer.toString());
+				
+			else if(qName.equals("Titre_de_l_oeuvre"))
+				oeuvre.get(indice).setTitre_de_l_oeuvre(buffer.toString());
+				
+			else if(qName.equals("Descriptif_de_l_oeuvre"))
+				oeuvre.get(indice).setDescriptif_de_l_oeuvre(buffer.toString());
+				
+			else if(qName.equals("Photographie_de_l_oeuvre_oui"))	
+				oeuvre.get(indice).setPhotographie_de_l_oeuvre_oui(buffer.toString());
+				
+			else if(qName.equals("Nom_de_l_architecte"))
+				oeuvre.get(indice).setNom_de_l_architecte(buffer.toString());
+				
+				
+			else if(qName.equals("Prenom_de_l_architecte"))
+				oeuvre.get(indice).setPrenom_de_l_architecte(buffer.toString());
+				
+			else if(qName.equals("Universite_Enseignement_sup_rieur"))
+				oeuvre.get(indice).setUniversite_Enseignement_sup_rieur(buffer.toString());
+				
+			}
+
+		@Override
+	public void startElement(String uri, String localName, String qName,
+		Attributes attributes) throws SAXException {
+					  	
+								if (qName.equals("Maliste") )
+				oeuvre.add(new ListeOeuvre());
+			    			
+			    else 
+			    	if (!qName.equals("Liste_Artiste"))
+			    	buffer = new StringBuilder();
+	         
+		}	
+			
+	public String getOriginal() {
+		return original;
+	}
+
+	public void setOriginal(String original) {
+		if (original == this.original)
+			return;
+		try {
+			// Ouverture du nouveau fichier XML
+			FIS = getClass().getResourceAsStream(original+".xml");
+			// Vidage des resultats de l'ancienne langue
+			oeuvre.clear();
+			// Lecture du nouveau fichier XML
+			saxParser.parse(FIS, this);
+			this.original = original;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	public ArrayList<ListeOeuvre> getOeuvre() {
+		return oeuvre;
+	}
+	public void setOeuvre(ArrayList<ListeOeuvre> oeuvre) {
+		this.oeuvre = oeuvre;
+	}
+			public static void main(final String[] args) {
+			 Parseur p =new Parseur("original");
+			 p.getOeuvre();
+			 
+					for(int i=0; i<p.getOeuvre().size();i++){
+			System.out.println("Nom"+":"+p.getOeuvre().get(i).getNom_de_l_artiste());
+			System.out.println("Prenom"+":"+p.getOeuvre().get(i).getPrenom_de_l_artiste());
+			System.out.println("Departement"+":"+p.getOeuvre().get(i).getDepartement());
+			System.out.println("Commune"+":"+p.getOeuvre().get(i).getCommune());
+			System.out.println("Nom de l'etablissement"+":"+p.getOeuvre().get(i).getNom_de_l_etablissement());
+			System.out.println("Region"+":"+p.getOeuvre().get(i).getRegion());
+
+				}
+	
+	}
+
+}
